672b98fd3f5894d9d72833e3e830c94b269be3c4
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-05-30 23:20:58 -0400

    sav auth bugs fixed

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://DennisTrujilloDev:hiHello@cluster0.olwy9tk.mongodb.net/?retryWrites=true&w=majority',
+    'dbName': 'savAuth'
+};
